5e2651e8fc480c2564f2a38d4a0222bcd1b831d5
AbnerOliveira <164270594+takamae1907@users.noreply.github.com>
2025-05-24 17:19:08 -0300

    Add files via upload

--- java.js
+++ java.js
@@ -7,20 +7,19 @@ window.addEventListener('scroll', function () {
     }
 });
 if (window.innerWidth > 500) {
-  
-window.sr = ScrollReveal({reset: true});
-sr.reveal('.tituloprincipal2', { duration:1300});
-sr.reveal('.bannerin', {duration:1200});
-sr.reveal('.img1', {duration:1900});
-sr.reveal('.cardimg', {duration:1200});
-sr.reveal('.cardtext', {duration:1900});
-sr.reveal('.h1text', {duration:1200});
-sr.reveal('.principal', {duration:1900});
-sr.reveal('.meublog', {duration:1200});
-sr.reveal('.blo', {duration:1900});
-sr.reveal('.baixoclass', {duration:2100});
-sr.reveal('.servicos', {duration:1200});
-sr.reveal('.centro', {duration:1200});
-sr.reveal('.image-section2', {duration:1200});
+  window.sr = ScrollReveal({ reset: true });
 
+  sr.reveal('.tituloprincipal2', { duration: 1300, origin: 'top', distance: '20px' });
+  sr.reveal('.bannerin', { duration: 1200, origin: 'top', distance: '20px' });
+  sr.reveal('.img1', { duration: 1900, origin: 'top', distance: '20px' });
+  sr.reveal('.cardimg', { duration: 1200, origin: 'top', distance: '20px' });
+  sr.reveal('.cardtext', { duration: 1900, origin: 'top', distance: '20px' });
+  sr.reveal('.h1text', { duration: 1200, origin: 'top', distance: '20px' });
+  sr.reveal('.principal', { duration: 1900, origin: 'top', distance: '20px' });
+  sr.reveal('.meublog', { duration: 1200, origin: 'top', distance: '20px' });
+  sr.reveal('.blo', { duration: 1900, origin: 'top', distance: '20px' });
+  sr.reveal('.baixoclass', { duration: 2100, origin: 'top', distance: '20px' });
+  sr.reveal('.servicos', { duration: 1200, origin: 'top', distance: '20px' });
+  sr.reveal('.centro', { duration: 1200, origin: 'top', distance: '20px' });
+  sr.reveal('.image-section2', { duration: 1200, origin: 'top', distance: '20px' });
 }
